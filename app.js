a334aebd3e674d49aaa2980ee3ac0402bf89f355
unknown <gabeclementej@gmail.com>
2022-03-14 19:51:51 -0300

    feat get data API

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8877;
 
-app.listen(PORT, () => {
- console.log('escutando na porta' + PORT)
-})
+app.get("/series", (req, res) => {
+  res.json({
+    exemplo: "exemplo",
+  });
+}),
+  app.get("/about", (req, res) => {
+    res.json({
+      name: "Gabriela Clemente",
+      email: "gabeclementej@gmail.com",
+      urls: [
+        {
+          type: "github",
+          url: "https://github.com/gabrielajc",
+        },
+        {
+          type: "linkedin",
+          url: "https://www.linkedin.com/in/gabeclemente",
+        },
+      ],
+    });
+  }),
+  app.get("/", (req, res) => {
+    res.json({
+      msg: "Working",
+    });
+  }),
+  app.listen(PORT, () => {
+    console.log("escutando na porta" + PORT);
+  });
