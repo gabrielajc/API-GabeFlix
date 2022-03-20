ecd913f3c7df4c7e6f5ffee3081ff2811b54d028
unknown <gabeclementej@gmail.com>
2022-03-20 12:55:30 -0300

    feat multer

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
 const express = require("express");
 const app = express();
+const uploadUser = require('./middlewares/uploadimage')
 
 const PORT = process.env.PORT || 8877;
+
+app.post("/upload-image", uploadUser.single('image'), async (req, res) => {
+
+  if(req.file){
+    console.log(req.file)
+    return res.json({
+      error: false,
+      msg:  "Upload image works"
+    });
+    
+  }
+
+  return res.status(400).json({
+    error: true,
+    msg: "Erro: upload image request don't works, necessary to send an image in png, jpeg or jpg format"
+  });
+
+});
 
 app.get("/series", (req, res) => {
   res.json({
     exemplo: "exemplo",
   });
+})
+
+app.get("/about", (req, res) => {
+  res.json({
+    name: "Gabriela Clemente",
+    email: "gabeclementej@gmail.com",
+    urls: [
+      {
+        type: "github",
+        url: "https://github.com/gabrielajc",
+      },
+      {
+        type: "linkedin",
+        url: "https://www.linkedin.com/in/gabeclemente",
+      },
+    ],
+  });
 }),
-  app.get("/about", (req, res) => {
-    res.json({
-      name: "Gabriela Clemente",
-      email: "gabeclementej@gmail.com",
-      urls: [
-        {
-          type: "github",
-          url: "https://github.com/gabrielajc",
-        },
-        {
-          type: "linkedin",
-          url: "https://www.linkedin.com/in/gabeclemente",
-        },
-      ],
-    });
-  }),
   app.get("/", (req, res) => {
     res.json({
       msg: "Working",
