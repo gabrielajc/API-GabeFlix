ee7be95e8baa1891fa2e8203ee31a47e0812b7ab
unknown <gabeclementej@gmail.com>
2022-03-20 15:07:20 -0300

    fix uploads

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 const express = require("express");
 const app = express();
-const uploadUser = require('./middlewares/uploadimage')
+const uploadUser = require("./middlewares/uploadimage");
+const path = require("path");
 
 const PORT = process.env.PORT || 8877;
 
-app.post("/upload-image", uploadUser.single('image'), async (req, res) => {
 
-  if(req.file){
-    console.log(req.file)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join("./public/uploads/series")));
+app.listen(PORT, () => {
+  console.log("escutando na porta" + PORT);
+});
+
+app.post("/upload-image", uploadUser.single("image"), async (req, res) => {
+  if (req.file) {
+    console.log(req.file);
     return res.json({
+      url: `localhost:8877/uploads/${req.file.filename}`,
       error: false,
-      msg:  "Upload image works"
+      msg: "Upload image works",
     });
-    
   }
 
   return res.status(400).json({
     error: true,
-    msg: "Erro: upload image request don't works, necessary to send an image in png, jpeg or jpg format"
+    msg: "Erro: upload image request don't works, necessary to send an image in png, jpeg or jpg format",
   });
-
 });
-
-app.get("/series", (req, res) => {
-  res.json({
-    exemplo: "exemplo",
-  });
-})
 
 app.get("/about", (req, res) => {
   res.json({
@@ -48,7 +49,4 @@ app.get("/about", (req, res) => {
     res.json({
       msg: "Working",
     });
-  }),
-  app.listen(PORT, () => {
-    console.log("escutando na porta" + PORT);
   });
